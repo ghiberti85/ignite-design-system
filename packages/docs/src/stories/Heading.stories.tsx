555f9646fa4e8c4743cefe7a0c5255e3c80515f3
ghiberti85 <ghiberti85@gmail.com>
2024-02-19 17:18:23 -0300

    Component: Heading

---
+++ packages/docs/src/stories/Heading.stories.tsx
@@ -0,0 +1,28 @@
+import { StoryObj, Meta } from '@storybook/react'
+import { Heading, HeadingProps } from '@ignite-ui/react'
+
+export default {
+  title: 'Typography/Heading',
+  component: Heading,
+  tags: ['autodocs'],
+  args: {
+    children: 'Title with Heading',
+  },
+} as Meta<HeadingProps>
+
+export const Primary: StoryObj<HeadingProps> = {}
+
+export const CustomTag: StoryObj<HeadingProps> = {
+  args: {
+    children: 'H1 Heading',
+    as: 'h1',
+  },
+  parameters: {
+    docs: {
+      description: {
+        story:
+          'Heading by default will be `h2`, but you can add a prop `as` with yout custom tag',
+      },
+    },
+  },
+}
